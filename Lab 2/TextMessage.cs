f78abe228437fc55c3fd46a5b8a1a581fa6621dd
Viktoriia Kobal <161163769+VikkiKobal@users.noreply.github.com>
2024-09-21 15:24:28 +0300

    Update TextMessage.cs

--- Lab 2/TextMessage.cs
+++ Lab 2/TextMessage.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 
+/*Клас Текстове повідомлення (текст повідомлення, час відправки, номер отримувача) */
+
+    public class TextMessage
+    {
+    public string Text { get; set; }
+    public DateTime SendTime { get; set; }
+    public string ReceiverNumber { get; set; }
+
+    public TextMessage(string text, DateTime sendTime, string receiverNumber)
+    {
+        Text = text;
+        SendTime = sendTime;
+        ReceiverNumber = receiverNumber;
+    }
+}
