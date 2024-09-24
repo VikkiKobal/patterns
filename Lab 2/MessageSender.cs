7ef87781d712b558d019acde80f4ecd71edd55a4
Viktoriia Kobal <161163769+VikkiKobal@users.noreply.github.com>
2024-09-24 10:20:07 +0300

    Update MessageSender.cs

--- Lab 2/MessageSender.cs
+++ Lab 2/MessageSender.cs
@@ -15,6 +15,35 @@
         bool SendMessage(TextMessage message);
     }
 
+    public class MessageProxy : IMessageSender
+    {
+    private readonly IMessageSender _messageSender;
+    private readonly string _filePath;
+
+    public MessageProxy(IMessageSender messageSender, string filePath)
+    {
+        _messageSender = messageSender;
+        _filePath = filePath;
+    }
+
+    public bool SendMessage(TextMessage message)
+    {
+        bool result = _messageSender.SendMessage(message);
+        SaveMessageToFile(message);
+        return result;
+    }
+
+    private void SaveMessageToFile(TextMessage message)
+    {
+        using (StreamWriter writer = File.AppendText(_filePath))
+        {
+            writer.WriteLine($"Message sent at {message.SendTime}: {message.Text} to {message.ReceiverNumber}");
+        }
+    }
+}
+
+
+
     public class MessageSender : IMessageSender
     {
         public bool SendMessage(TextMessage message)
@@ -33,40 +62,40 @@
             }
         }
     }
-        public class Blacklist
+    public class Blacklist
+    {
+        private readonly List<string> _blacklistedNumbers;
+
+        public Blacklist(List<string> blacklist)
         {
-            private readonly List<string> _blacklistedNumbers;
-
-            public Blacklist(List<string> blacklist)
-            {
-                _blacklistedNumbers = new List<string>(blacklist);
-            }
-
-            public bool IsBlacklisted(string receiverNumber)
-            {
-                return _blacklistedNumbers.Contains(receiverNumber);
-            }
+            _blacklistedNumbers = new List<string>(blacklist);
         }
+
+        public bool IsBlacklisted(string receiverNumber)
+        {
+            return _blacklistedNumbers.Contains(receiverNumber);
+        }
+    }
 
 
     public class MessageSenderProxy : IMessageSender
+    {
+        private readonly IMessageSender _messageSender;
+        private readonly Blacklist _blacklist;
+
+        public MessageSenderProxy(IMessageSender messageSender, Blacklist blacklist)
         {
-            private readonly IMessageSender _messageSender;
-            private readonly Blacklist _blacklist;
-
-            public MessageSenderProxy(IMessageSender messageSender, Blacklist blacklist)
-            {
-                _messageSender = messageSender;
-                _blacklist = blacklist;
-            }
-
-            public bool SendMessage(TextMessage message)
-            {
-                if (_blacklist.IsBlacklisted(message.ReceiverNumber))
-                {
-                    throw new BlacklistException(message.ReceiverNumber);
-                }
-
-                return _messageSender.SendMessage(message);
-            }
+            _messageSender = messageSender;
+            _blacklist = blacklist;
         }
+
+        public bool SendMessage(TextMessage message)
+        {
+            if (_blacklist.IsBlacklisted(message.ReceiverNumber))
+            {
+                throw new BlacklistException(message.ReceiverNumber);
+            }
+
+            return _messageSender.SendMessage(message);
+        }
+    }
